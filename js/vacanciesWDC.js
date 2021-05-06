dc5494233104df63a9d56317396949896a7e5f99
iskander <oldautist@gmail.com>
2021-05-06 11:27:49 +0300

    id

--- js/vacanciesWDC.js
+++ js/vacanciesWDC.js
@@ -112,7 +112,7 @@
 
     // Download the data
     myConnector.getData = function(table, doneCallback) {
-        $.getJSON("https://drive.google.com/file/d/1_ehbi1qrhgjsXnhsKFpuxY-EqH5nm0Gr/view?usp=sharing", function(resp) {
+        $.getJSON("https://drive.google.com/file/d/1_SDT07iquU9qAa34svtBvqoytbDRBsos", function(resp) {
         var feat = resp.features,
             tableData = [];
 
